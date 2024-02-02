13d2d6c55aa182470b75e8d58160c6c8dca82582
janthony25 <junanthony.business@gmail.com>
2024-02-03 09:24:08 +1300

    Add project files.

---
+++ OOP-Static/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ******** IMPORTANT NOTE **********
+            // static = modifier to declare a static member, which belongs to the class itself
+            //          rather than to any specific object
+
+
+            Car car1 = new Car("Mustang");
+            Car car2 = new Car("Corvette");
+            Car car3 = new Car("Civic");
+
+            Console.WriteLine(Car.numberOfCars);
+
+            
+            Car.StartRace();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Car
+    {
+        string model;
+        public static int numberOfCars;
+
+        // Constructor
+        public Car(string model)
+        {
+            this.model = model;
+            numberOfCars++;
+        }
+
+        // static object
+        public static void StartRace()
+        {
+            Console.WriteLine("The race has now begun!");
+        }
+
+
+    }
+}
